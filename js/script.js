0c6b708ec4979b90b8eed9ebd15e1373d095974e
VineethKiniK <kinivineeth2001@gmail.com>
2023-11-06 07:23:12 -0800

    TODO

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const addButton = document.getElementById("add-button");
+
+addButton.addEventListener("click", addTask);
+
+function addTask(){
+    console.log("enter");
+    if(inputBox.value === ''){
+        alert("Please write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    inputBox.value = '';
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
